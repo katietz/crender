aaa74d3e86a283bf287b15aaf9bfa11cb93693d5
Kai Tietz <ktietz@anaconda.com>
2021-05-29 06:45:37 +0200

    add g++14 as standard

--- bld.sh
+++ bld.sh
@@ -6,6 +6,6 @@ mkdir -p bin
 cp data/conda_build_config.yaml bin/.
 
 cd src
-g++ -o ../bin/crender main.cc prepro.cc yaml.cc lint.cc -L ../pref/lib/static/ -L../pref/lib -I ../pref/include/ -ljinja2cpp -lpthread -I ../yaml-cpp/include/ -lyaml-cpp
+g++ --std=gnu++14 -o ../bin/crender main.cc prepro.cc yaml.cc lint.cc -L ../pref/lib/static/ -L../pref/lib -I ../pref/include/ -ljinja2cpp -lpthread -I ../yaml-cpp/include/ -lyaml-cpp
 cd ..
 
